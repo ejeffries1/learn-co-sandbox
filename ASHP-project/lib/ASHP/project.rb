9b756c4ec474fa36b273ed35cc4f57f4498c90b8
Evan Jeffries <ejeffries14@gmail.com>
2019-10-28 18:07:40 +0000

    first commit

---
+++ ASHP-project/lib/ASHP/project.rb
@@ -0,0 +1,8 @@
+require "ASHP/project/version"
+
+module ASHP
+  module Project
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ ASHP-project/lib/ASHP/project/version.rb
@@ -0,0 +1,5 @@
+module ASHP
+  module Project
+    VERSION = "0.1.0"
+  end
+end
